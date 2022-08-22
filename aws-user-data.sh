c53feae7e9cd0075573e95d91c2fd9791b644f8b
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2022-08-22 16:59:09 +0200

    Update aws-user-data.sh

--- aws-user-data.sh
+++ aws-user-data.sh
@@ -7,3 +7,9 @@ echo "<h1>AWS Virtual Machine deployed with Terraform</h1>" | sudo tee /var/www/
 yum install -y python3
 yum install python3-pip -y
 sudo yum install git
+cd /tmp
+sudo git clone https://github.com/Rodi26/aws-wiki"
+cd /tmp/aws-wiki
+sudo pip3 install mkdocs-material
+sudo pip3 install -r ./requirements.txt
+sudo  mkdocs serve
