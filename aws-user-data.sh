b3367189e5ebd3aaa7e7f74e686816d3ed22df5e
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2022-08-22 16:57:26 +0200

    Update aws-user-data.sh

--- aws-user-data.sh
+++ aws-user-data.sh
@@ -4,3 +4,6 @@ sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 echo "<h1>AWS Virtual Machine deployed with Terraform</h1>" | sudo tee /var/www/html/index.html
+yum install -y python3
+yum install python3-pip -y
+sudo yum install git
